d22a552e7fd75066e42d15880fe813b6014221f2
tehkesihwindy <tehkesihwindy@tehkesihs-mbp>
2018-01-22 20:13:14 +0700

    This is my project

---
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -0,0 +1,45 @@
+package com.pluralsight.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pluralsight.model.Activity;
+import com.pluralsight.model.Exercise;
+
+
+@Controller
+public class MinutesController {
+
+	@RequestMapping(value = "/addMinutes")
+	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+		
+		System.out.println("exercise: " + exercise.getMinutes());
+		
+		return "addMinutes";
+	}
+	
+	@RequestMapping(value="/activities", method= RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities(){
+		List<Activity> activities = new ArrayList<Activity>();
+		
+		Activity run = new Activity();
+		run.setDesc("Run");
+		activities.add(run);
+		
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		activities.add(bike);
+		
+		Activity swim = new Activity();
+		swim.setDesc("Swim");
+		activities.add(swim);
+		
+		return activities;
+	}
+}
